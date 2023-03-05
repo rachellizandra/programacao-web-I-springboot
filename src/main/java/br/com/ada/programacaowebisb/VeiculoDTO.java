be83d1c4821c4b8fe288b6de279770f072da8ee5
Rachel <rachellizandra@outlook.com>
2023-03-05 20:09:12 -0300

    testando repositório no linux

--- src/main/java/br/com/ada/programacaowebisb/VeiculoDTO.java
+++ src/main/java/br/com/ada/programacaowebisb/VeiculoDTO.java
@@ -1,8 +1,15 @@
 package br.com.ada.programacaowebisb;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class VeiculoDTO {
     private String placa;
     private String marca;
+
+    //@NotBlank(message = "Modelo não pode ser vazio")
     private String modelo;
     private String tipo;
     private Boolean disponivel;
--- src/main/java/br/com/ada/programacaowebisb/controller/VeiculoController.java
+++ src/main/java/br/com/ada/programacaowebisb/controller/VeiculoController.java
@@ -1,10 +1,12 @@
 package br.com.ada.programacaowebisb.controller;
 
+import br.com.ada.programacaowebisb.VeiculoDTO;
 import br.com.ada.programacaowebisb.model.Veiculo;
 import br.com.ada.programacaowebisb.service.VeiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,16 +20,22 @@ public class VeiculoController {
     private VeiculoService veiculoService;
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<String> createVeiculo(@RequestBody Veiculo veiculo) {
+    public ResponseEntity<String> createVeiculo(@Validated @RequestBody VeiculoDTO veiculo) {
         try{
-            this.veiculoService.createVeiculo(veiculo);
+            Veiculo veiculoDB = Veiculo.builder()
+                    .placa(veiculo.getPlaca())
+                    .modelo(veiculo.getModelo())
+                    .marca(veiculo.getMarca())
+                    .tipo(veiculo.getTipo())
+                    .disponivel(veiculo.getDisponivel())
+                    .build();
+            this.veiculoService.createVeiculo(veiculoDB);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body("Veículo cadastrado!");
         }catch (Exception e){
             return ResponseEntity.badRequest().build();
         }
-
     }
 
     @GetMapping("/listar")
