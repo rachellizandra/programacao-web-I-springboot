8f123238f153aa1d04177f77f4ba8ba02d6bdc60
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 14:49:52 -0300

    altera a interface repository

--- src/main/java/br/com/ada/programacaowebisb/repository/VeiculoRepository.java
+++ src/main/java/br/com/ada/programacaowebisb/repository/VeiculoRepository.java
@@ -4,7 +4,9 @@ import br.com.ada.programacaowebisb.model.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository //se comunica com a base de dados (model)
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
-
+    Optional<Veiculo> findByPlacaContaining(String placa);
 }
