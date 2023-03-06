dbcba54ad54d92455b9208682305d6f4a8c859e1
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 14:48:08 -0300

    adiciona a controller em MVC para interagir com o front-end

--- src/main/java/br/com/ada/programacaowebisb/controller/VeiculoController.java
+++ src/main/java/br/com/ada/programacaowebisb/controller/VeiculoController.java
@@ -1,7 +1,9 @@
 package br.com.ada.programacaowebisb.controller;
 
+import br.com.ada.programacaowebisb.rest.dto.VeiculoDTO;
 import br.com.ada.programacaowebisb.model.Veiculo;
 import br.com.ada.programacaowebisb.service.VeiculoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +19,25 @@ public class VeiculoController {
     @Autowired
     private VeiculoService veiculoService;
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<String> createVeiculo(@RequestBody Veiculo veiculo) {
-        try{
-            this.veiculoService.createVeiculo(veiculo);
+    @PostMapping("/cadastrar") //POST
+    public ResponseEntity<String> createVeiculo(@Valid @RequestBody VeiculoDTO veiculo) {
+        try {
+
+            Veiculo veiculoDB = Veiculo.builder()
+                    .placa(veiculo.getPlaca())
+                    .modelo(veiculo.getModelo())
+                    .disponivel(veiculo.getDisponivel())
+                    .tipo(veiculo.getTipo())
+                    .build();
+
+
+            this.veiculoService.createVeiculo(veiculoDB);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
-                    .body("Veículo cadastrado!");
-        }catch (Exception e){
+                    .body("Veículo criado!");
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
-
     }
 
     @GetMapping("/listar")
@@ -43,6 +53,40 @@ public class VeiculoController {
             return ResponseEntity.ok(optionalVeiculo.get());
         }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
+    @PutMapping("/atualizar")
+    public ResponseEntity<String> atualizarVeiculo(@RequestBody VeiculoDTO veiculo) {
+        //1 - buscar veiculo pela placa
+        //2 - atualizar veiculo
+
+        try {
+            Optional<Veiculo> optionalVeiculo = this.veiculoService.buscarVeiculoPlaca(veiculo.getPlaca());
+
+            if (optionalVeiculo.isPresent()) {
+                Veiculo veiculoPorPlacaDB = optionalVeiculo.get();
+                Veiculo veiculoAtualizar = Veiculo.builder().id(veiculoPorPlacaDB.getId())
+                        .modelo(veiculo.getModelo())
+                        .marca(veiculo.getMarca())
+                        .placa(veiculo.getPlaca())
+                        .tipo(veiculo.getTipo())
+                        .disponivel(veiculo.getDisponivel())
+                        .build();
+                this.veiculoService.createVeiculo(veiculoAtualizar);
+
+                return ResponseEntity
+                        .ok("Veículo atualizado!");
+            }
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void removerVeiculo(@PathVariable Long id){
+        this.veiculoService.removerVeiculoPorId(id);
     }
 }
--- src/main/java/br/com/ada/programacaowebisb/controller/VeiculoControllerMVC.java
+++ src/main/java/br/com/ada/programacaowebisb/controller/VeiculoControllerMVC.java
@@ -1,84 +1,41 @@
 package br.com.ada.programacaowebisb.controller;
 
-import br.com.ada.programacaowebisb.VeiculoDTO;
 import br.com.ada.programacaowebisb.model.Veiculo;
+import br.com.ada.programacaowebisb.rest.dto.VeiculoDTO;
 import br.com.ada.programacaowebisb.service.VeiculoService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-//@Controller
-@RequestMapping("/veiculo")
+//@RestController
+@Controller
 public class VeiculoControllerMVC {
     @Autowired
     private VeiculoService veiculoService;
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<String> createVeiculo(@Valid @RequestBody Veiculo veiculo) {
-        try{
-            this.veiculoService.createVeiculo(veiculo);
-            return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body("Veículo cadastrado!");
-        }catch (Exception e){
-            return ResponseEntity.badRequest().build();
-        }
-
+    @GetMapping("/veiculos")
+    public String listarveiculos(Model model) {
+        List<Veiculo> veiculos = this.veiculoService.listarTodos();
+        model.addAttribute("veiculos", veiculos);
+        return "veiculos";
     }
 
-    @GetMapping("/listar")
-    public List<Veiculo> listarTodos(){
-        return this.veiculoService.listarTodos();
+    @GetMapping("/veiculo/add")
+    public String buscarVeiculo(Model model) {
+        model.addAttribute("veiculo", new Veiculo());
+        return "veiculo-add";
     }
 
-    @GetMapping("/by/{id}")
-    public ResponseEntity<Veiculo> listarVeiculoId(@PathVariable("id") Long id){
-        Optional<Veiculo> optionalVeiculo = this.veiculoService.listarVeiculoId(id);
-
-        if(optionalVeiculo.isPresent()){
-            return ResponseEntity.ok(optionalVeiculo.get());
-        }
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    @PostMapping("/veiculo/add")
+    public String criarVeiculo(@ModelAttribute("veiculo") Veiculo veiculo) {
+        this.veiculoService.createVeiculo(veiculo);
+        return "redirect:/veiculos";
     }
 
-    @PutMapping("/atualizar")
-    public ResponseEntity<String> atualizarVeiculo(@RequestBody VeiculoDTO veiculo) {
-        //1 - buscar veiculo pela placa
-        //2 - atualizar veiculo
-
-        try {
-            Optional<Veiculo> optionalVeiculo = this.veiculoService.buscarVeiculoPlaca(veiculo.getPlaca());
-
-            if (optionalVeiculo.isPresent()) {
-                Veiculo veiculoPorPlacaDB = optionalVeiculo.get();
-                Veiculo veiculoAtualizar = Veiculo.builder().id(veiculoPorPlacaDB.getId())
-                        .modelo(veiculo.getModelo())
-                        .marca(veiculo.getMarca())
-                        .placa(veiculo.getPlaca())
-                        .tipo(veiculo.getTipo())
-                        .disponivel(veiculo.getDisponivel())
-                        .build();
-                this.veiculoService.createVeiculo(veiculoAtualizar);
-
-                return ResponseEntity
-                        .ok("Veículo atualizado!");
-            }
-            return ResponseEntity
-                    .status(HttpStatus.NO_CONTENT).build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
-
-    }
-
-    @DeleteMapping("/deletar/{id}")
-    public void removerVeiculo(@PathVariable Long id){
-        this.veiculoService.removerVeiculoPorId(id);
-    }
 }
