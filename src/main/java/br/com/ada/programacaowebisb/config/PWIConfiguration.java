12e76d9a272ad01496547e5ff4a373bfb3b23590
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 14:46:50 -0300

    adiciona método getCarro() nas configurações

--- src/main/java/br/com/ada/programacaowebisb/config/PWIConfiguration.java
+++ src/main/java/br/com/ada/programacaowebisb/config/PWIConfiguration.java
@@ -1,2 +1,15 @@
-package br.com.ada.programacaowebisb.config;public class PWIConfiguration {
+package br.com.ada.programacaowebisb.config;
+
+import br.com.ada.programacaowebisb.model.Veiculo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PWIConfiguration {
+
+    @Bean
+    public Veiculo getCarro() {
+        return new Veiculo();
+    }
+
 }
